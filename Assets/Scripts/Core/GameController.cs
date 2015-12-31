c49675ae0bdee003fef63bf3ed41e0e9da49298c
MaximeEngel <maximeengel@gmail.com>
2015-12-31 18:39:33 +0100

    Start test environment and player movement

---
+++ Assets/Scripts/Core/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private Vector3 lastMousePosition;
+
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		this.lastMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ManagePlayerInput ();
+	}
+
+	private void ManagePlayerInput() {
+		// Linear movement
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		player.Move (x, y);
+
+		// Rotation Orientation
+		Vector3 mousePosition = Input.mousePosition;
+		float delta = mousePosition.x - lastMousePosition.x;
+		player.Rotate(delta);
+		this.lastMousePosition = mousePosition;
+
+	}
+}
---
+++ Assets/Scripts/Core/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 1;
+
+	private Vector3 move;
+	private float rotateY;
+	private Rigidbody rigidBody;
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidBody = GetComponent<Rigidbody> ();
+		this.move = new Vector3 ();
+		this.camera = GetComponentInChildren<Camera> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		Move ();
+		Rotate ();
+	}	
+
+	public void Move(float x, float y) {
+		this.move.x = x;
+		this.move.z = y;
+	}
+
+	private void Move() {
+		Debug.Log (this.move);
+		Vector3 cameraDirection = this.camera.transform.forward;
+		cameraDirection.Normalize ();
+		cameraDirection.Scale (move * speed);
+		this.rigidBody.velocity = cameraDirection;
+	}
+
+	public void Rotate(float x) {
+		this.rotateY = x;
+	}
+
+	private void Rotate() {
+		float angleY = this.rigidBody.rotation.eulerAngles.y;
+		this.rigidBody.rotation = Quaternion.Euler(0, angleY + this.rotateY, 0);
+	}
+}
