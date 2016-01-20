d4a723a91217a4e80c7f5adcd5b41c5029dc5f34
MrPaulin <paulineboisgallais@yahoo.fr>
2016-01-20 11:39:09 +0100

    Objects are now the right size and rotate

--- Assets/Scripts/Core/InventoryGUI.cs
+++ Assets/Scripts/Core/InventoryGUI.cs
@@ -8,15 +8,38 @@ public class InventoryGUI : MonoBehaviour {
 	// Where the object are stored when the inventory is closed
 	public GameObject playerHand;
 
+	public float slotSize = 0.5f;
 	public Inventory inventory;
 	public GameObject slotPanel;
+	public GameObject AssembleWindow;
 	private List<Transform> inventorySlots;
+	private bool isDragging;
+	private GameObject draggedObject;
 
 	void Awake(){
 		this.inventory = this.player.inventory;
 		this.inventorySlots = new List<Transform> (this.player.inventorySize);
+		this.isDragging = false;
+		this.draggedObject = null;
 		foreach (Transform slot in this.slotPanel.transform) {
-			inventorySlots.Add (slot);
+			// The list inventorySlots contains all the Scalers
+			inventorySlots.Add (slot.GetChild(0).GetChild(0));
+		}
+	}
+
+	void Update(){
+		if (this.isDragging) {
+			// Interpolate screen coordinates to match world coordinates 
+			float xMouseToWorld = Mathf.Lerp (-8.0f, 8.0f, Input.mousePosition.x/Screen.width);
+			float yMouseToWorld = Mathf.Lerp (-4.5f, 4.5f, Input.mousePosition.y/Screen.height);
+
+			this.draggedObject.transform.position = new Vector3 (xMouseToWorld,yMouseToWorld,0);
+		}
+		if (Input.GetButtonUp ("Interact")) {
+			//TODO
+		}
+		if (Input.GetButtonDown ("Interact")) {
+			//TODO
 		}
 	}
 
@@ -31,19 +54,28 @@ public class InventoryGUI : MonoBehaviour {
 	void GatherObjects(){
 		int slotCount = 0;
 		List<GameObject> objectsToMove = new List<GameObject> (this.player.inventorySize);
+
+		// Find all the origamiObjects that the player has
 		foreach(Transform childObj in this.playerHand.transform){
 			if(childObj.gameObject.tag == "OrigamiObject"){
-
 				objectsToMove.Add(childObj.gameObject);
 			}
 		}
+
+		// Move them all to their slot in the inventory
 		foreach (GameObject origamiObject in objectsToMove) {
-			origamiObject.transform.parent = this.inventorySlots [slotCount];
+			Transform currentSlot = this.inventorySlots [slotCount];
+			origamiObject.transform.parent = currentSlot;
 			origamiObject.gameObject.SetActive (true);
 			origamiObject.transform.localPosition = Vector3.zero;
+			origamiObject.transform.localRotation = Quaternion.identity;
 
-			// TODO : make the object fit the slot
-			//origamiObject.transform.localScale = ;
+			Vector3 origamiBounds = origamiObject.GetComponent<Renderer>().bounds.extents;
+			float maxBound = Mathf.Max (origamiBounds.x, origamiBounds.y, origamiBounds.z);
+			float scaleFactor = slotSize / maxBound;
+			currentSlot.localScale = scaleFactor*Vector3.one;
+
+			currentSlot.GetComponent<InventoryIdleAnimation>().isRotating = true;
 
 			origamiObject.gameObject.layer = (5);
 			slotCount++;
@@ -51,14 +83,13 @@ public class InventoryGUI : MonoBehaviour {
 	}
 
 	void ReturnObjects(){
-		foreach (Transform slot in this.slotPanel.transform) {
+		foreach (Transform slot in this.inventorySlots) {
 			if (slot.childCount > 0) {
+				slot.localScale = Vector3.one;
 				Transform origamiObject = slot.GetChild (0);
 				origamiObject.transform.parent = this.playerHand.transform;
 				origamiObject.gameObject.SetActive (false);
-
-				// TODO : reset scale (maybe place every OrigamiObject into a empty GameObject)
-				//origamiObject.transform.localScale = ;
+				slot.GetComponent<InventoryIdleAnimation> ().isRotating = false;
 
 				origamiObject.gameObject.layer = (0);
 
---
+++ Assets/Scripts/Core/InventoryIdleAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryIdleAnimation : MonoBehaviour {
+
+	public float returnSpeed = 4;
+	public bool isRotating = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isRotating)
+			this.transform.Rotate (Vector3.up);
+		else {
+			if (Mathf.Abs (this.transform.localEulerAngles.y) >= 1 && Mathf.Abs (this.transform.localEulerAngles.y)<=359) {
+				float newAngle = Mathf.LerpAngle (this.transform.localEulerAngles.y, 0, this.returnSpeed*Time.deltaTime);
+				this.transform.localEulerAngles = newAngle * Vector3.up;
+			}
+			else
+				this.transform.localRotation = Quaternion.identity;
+		}
+	}
+
+	public void StopRotation(){
+		this.isRotating = false;
+	}
+
+	public void ResumeRotation(){
+		this.isRotating = true;
+	}
+}
