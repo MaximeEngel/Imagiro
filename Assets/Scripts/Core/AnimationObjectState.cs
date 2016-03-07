d3d4c7109bf429b8412754af9d95160b2e4f7fba
MaximeEngel <maximeengel@gmail.com>
2016-03-07 10:36:23 +0100

    Add AnimationObjectState

---
+++ Assets/Scripts/Core/AnimationObjectState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationObjectState : ObjectState {
+
+	public Animator animator;
+	public int nbState = 2;
+	public bool loop = true;
+	public bool reverseLoop = true;
+
+	private int state;
+	private int step;
+
+	// Use this for initialization
+	void Start () {
+		this.state = 0;
+		this.step = 1;
+	}
+
+	public override void Action ()
+	{
+		if (this.state == this.nbState - 1 && this.step > 0 || this.state == 0 && this.step < 0) {
+			if (this.loop) { 
+				this.step *= -1;
+			} else {
+				return;
+			}
+		}
+		this.state = Mathf.Clamp (this.state + this.step, 0, this.nbState - 1);
+		this.animator.SetInteger ("state", this.state);
+	}
+
+	public override void InstantAction ()
+	{
+		
+	}
+}
