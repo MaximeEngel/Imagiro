88d155c94a12204929a50a470a7f50c29c2985c6
MrPaulin <paulineboisgallais@yahoo.fr>
2016-03-12 19:36:17 +0100

    Clicking on buttons can make you change menus, load first scene (but with texture errors)

---
+++ Assets/Scripts/Menu/menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	public RectTransform mainMenu;
+	public RectTransform levelSelection;
+	public string startScene;
+	public string[] levels;
+
+	// Use this for initialization
+	void Start () {
+		mainMenu.gameObject.SetActive (true);
+		levelSelection.gameObject.SetActive (false);
+	}
+	
+	public void GoToLevelSelection(){
+		mainMenu.gameObject.SetActive (false);
+		levelSelection.gameObject.SetActive (true);
+	}
+
+	public void GoToLevelMainMenu(){
+		mainMenu.gameObject.SetActive (true);
+		levelSelection.gameObject.SetActive (false);
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene (startScene);
+	}
+
+	public void StartLevel(int idx){
+		SceneManager.LoadScene(levels[idx]);
+	}
+}
