ab676be1a1fa30165d65ee55c4bc3f65d711b102
MrPaulin <paulineboisgallais@yahoo.fr>
2016-03-12 20:16:58 +0100

    added a simple skybox and camera rotation

---
+++ Assets/Scripts/Menu/camRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camRotation : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.localRotation = Quaternion.AngleAxis (speed * Time.time,Vector3.up);
+	}
+}
