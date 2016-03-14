a2bee9b2bdda12cc0259e275ead50e7f728c1ed8
kouglov <kouglov8@gmail.com>
2016-03-14 16:44:10 +0100

    Room 2 anchorPoints done and lever modification

---
+++ Assets/Scripts/Room2/Coal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coal : ObjectAction {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void Action (int numState)
+	{
+		Debug.Log ("COUCOU");
+
+	}
+
+	public override void InstantAction (int numState)
+	{
+		throw new System.NotImplementedException ();
+	}
+}
