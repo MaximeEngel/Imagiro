6f5c711af86b3e3003b7175c2e32f5e1f692da13
kouglov <kouglov8@gmail.com>
2016-03-17 13:18:34 +0100

    Added fire and main script

---
+++ Assets/Scripts/Room2/Main.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	public TargetObject Coal;
+	public TargetObject Rock;
+
+	public Light Red;
+	public Light Green;
+	public Light Blue;
+
+	public int Lever1State;
+	public int Lever2State;
+
+	// Use this for initialization
+	void Start () {
+		Coal =  GameObject.Find("TargetObjectCoal").GetComponent<TargetObject>();
+		Rock =  GameObject.Find("TargetObjectRock").GetComponent<TargetObject>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//print (Coal.name);
+		//print (Rock.name);
+
+	
+	}
+}
