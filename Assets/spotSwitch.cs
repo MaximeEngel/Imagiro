37b51210f628a7b2ef3bf68c9152988433856305
MrPaulin <paulineboisgallais@yahoo.fr>
2016-03-21 22:16:07 +0100

    Scene almost over, except two textures to add

---
+++ Assets/spotSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spotSwitch : ObjectAction {
+
+	public override void Action (int numState){
+		Debug.Log ("Je suis dans la lumière");
+		this.GetComponent<Light> ().enabled = !this.GetComponent<Light> ().enabled;
+	}
+
+	public override void InstantAction (int numState){
+	}
+}
