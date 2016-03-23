602a10262896cf9232138af36948e5c641303239
MrPaulin <paulineboisgallais@yahoo.fr>
2016-03-23 15:40:11 +0100

    Added the VR helpScreen

---
+++ Assets/Scripts/Core/Menu/helpPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class helpPanel : MonoBehaviour {
+
+	public VRMenuController vrController;
+	public RectTransform vrPanel;
+	public RectTransform keyBoardPanel;
+
+	// Use this for initialization
+	void Start () {
+		if (vrController.VRMode) {
+			vrPanel.gameObject.SetActive (true);
+			keyBoardPanel.gameObject.SetActive (false);
+		} else {
+			vrPanel.gameObject.SetActive (false);
+			keyBoardPanel.gameObject.SetActive (true);
+		}
+	}
+}
